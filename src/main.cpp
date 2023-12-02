ad7d653742202ae40261b0f00da9371ec20d5795
Error_Account <66193775+sarojlol@users.noreply.github.com>
2023-12-02 23:42:14 +0700

    modified:   platformio.ini 	modified:   src/main.cpp 	modified:   src/pin_define.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
 #include <pin_define.h>
 #include <stepper.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void task_setup();
 void pin_setup();
+void lcd_setup();
+void home_screen();
 
 bool startSW_flag;
 bool startSW_state;
-bool counting_toggle = true;
+int counting_stage;
 int start_flag;
 int stall_count;
 
@@ -16,13 +21,16 @@ bool sensor_data[4];
 bool sensor_flag[4];
 unsigned long sensor_filter[4];
 int raw_baht[4] = {-1, -1, -1, -1};
+int total;
+int last_total; 
 
 void setup() 
 {
   Serial.begin(115200);
+  pin_setup();
   tmc_setup();
   task_setup();
-  pin_setup();
+  lcd_setup();
 }
 
 void loop() {
@@ -35,7 +43,7 @@ void motor_handle(void * pvparameter){
     motor_run();
     static unsigned long afterStall_delay;
     static unsigned long reverse_delay;
-    if (counting_toggle)
+    if (counting_stage == 1)
     {
       //notmal operation
       if (!motor_stalled() && start_flag == 0)
@@ -83,7 +91,12 @@ void motor_handle(void * pvparameter){
         {
           stall_count = 5;
           motor_stop();
-          counting_toggle = false;
+          counting_stage = 2;
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.print("Jammed! Please Clear Stuck Coin");
+          lcd.setCursor(0,1);
+          lcd.print("Then Press Start/Stop Button");
           startSW_flag = true; 
         }
         else if ((millis() - afterStall_delay) > 1000)
@@ -114,7 +127,22 @@ void button_handle(void * pvparameter)
     startSW_state = digitalRead(start_SW);
     if (startSW_state == LOW &! startSW_flag)
     {
-      counting_toggle =! counting_toggle;
+      switch (counting_stage)
+      {
+      case 0:
+        counting_stage = 1;
+        home_screen();
+        break;
+      case 1:
+        counting_stage = 0;
+        home_screen();
+        break;
+      case 2:
+        counting_stage = 0;
+        home_screen();
+        digitalWrite(motor_led, LOW);
+        break;
+      }
       stall_count = 0;
       start_flag = 0;
       startSW_flag = true;
@@ -125,7 +153,7 @@ void button_handle(void * pvparameter)
     }
 
     //stop counting
-    else if (!counting_toggle && startSW_flag)
+    else if ((counting_stage == 0 || counting_stage == 2) && startSW_flag)
     {
       motor_stop();
       digitalWrite(motor_led, LOW);
@@ -139,80 +167,110 @@ void sensor_handle(void * pvparameter)
 {
   for(;;)
   {
-    for (int i=0; i<4; i++){sensor_data[i] = digitalRead(sensor_pin[i]);} //read all sensor state
+    if (counting_stage == 1)
+    {
+      for (int i=0; i<4; i++){sensor_data[i] = digitalRead(sensor_pin[i]);} //read all sensor state
 
-    //1 baht
-    if ((!sensor_data[0]) &! (sensor_flag[0]))
-    {
-      raw_baht[0] += 1;
-      sensor_filter[0] = millis();
-      sensor_flag[0] = true;
-    }
-    else if ((sensor_data[0]) && (sensor_flag[0]))
-    {
-      if ((millis() - sensor_filter[0]) > 10)
+      //1 baht
+      if ((!sensor_data[0]) &! (sensor_flag[0]))
       {
-        sensor_flag[0] = false;
+        raw_baht[0] ++;
+        sensor_filter[0] = millis();
+        lcd.setCursor(2, 0);
+        lcd.print(raw_baht[0]);
+        sensor_flag[0] = true;
       }
-    }
-
-    //2 baht
-    if ((!sensor_data[1]) &! (sensor_flag[1]))
-    {
-      raw_baht[1] += 2;
-      sensor_filter[1] = millis();
-      sensor_flag[1] = true;
-    }
-    else if ((sensor_data[1]) && (sensor_flag[1]))
-    {
-      if ((millis() - sensor_filter[1]) > 10)
+      else if ((sensor_data[0]) && (sensor_flag[0]))
       {
-        sensor_flag[1] = false;
+        if ((millis() - sensor_filter[0]) > 20)
+        {
+          sensor_flag[0] = false;
+        }
       }
-    }
 
-    //5 baht
-    if ((!sensor_data[2]) &! (sensor_flag[2]))
-    {
-      raw_baht[2] += 5;
-      sensor_filter[2] = millis();
-      sensor_flag[2] = true;
-    }
-    else if ((sensor_data[2]) && (sensor_flag[2]))
-    {
-      if ((millis() - sensor_filter[2]) > 10)
+      //2 baht
+      if ((!sensor_data[1]) &! (sensor_flag[1]))
       {
-        sensor_flag[2] = false;
+        raw_baht[1] ++;
+        sensor_filter[1] = millis();
+        lcd.setCursor(7,0);
+        lcd.print(raw_baht[1]);
+        sensor_flag[1] = true;
       }
-    }
-
-    //10 baht
-    if ((!sensor_data[3]) &! (sensor_flag[3]))
-    {
-      raw_baht[3] += 10;
-      sensor_filter[3] = millis();
-      sensor_flag[3] = true;
-    }
-    else if ((sensor_data[3]) && (sensor_flag[3]))
-    {
-      if ((millis() - sensor_filter[3]) > 10)
+      else if ((sensor_data[1]) && (sensor_flag[1]))
       {
-        sensor_flag[3] = false;
+        if ((millis() - sensor_filter[1]) > 20)
+        {
+          sensor_flag[1] = false;
+        }
+      }
+
+      //5 baht
+      if ((!sensor_data[2]) &! (sensor_flag[2]))
+      {
+        raw_baht[2] ++;
+        sensor_filter[2] = millis();
+        lcd.setCursor(13,0);
+        lcd.print(raw_baht[2]);
+        sensor_flag[2] = true;
+      }
+      else if ((sensor_data[2]) && (sensor_flag[2]))
+      {
+        if ((millis() - sensor_filter[2]) > 20)
+        {
+          sensor_flag[2] = false;
+        }
+      }
+
+      //10 baht
+      if ((!sensor_data[3]) &! (sensor_flag[3]))
+      {
+        raw_baht[3] ++;
+        sensor_filter[3] = millis();
+        lcd.setCursor(3, 1);
+        lcd.print(raw_baht[3]);
+        sensor_flag[3] = true;
+      }
+      else if ((sensor_data[3]) && (sensor_flag[3]))
+      {
+        if ((millis() - sensor_filter[3]) > 20)
+        {
+          sensor_flag[3] = false;
+        }
+      }
+
+      total = raw_baht[0] + (raw_baht[1] * 1) + (raw_baht[2] * 5) + (raw_baht[3] * 10);
+      if (last_total != total){
+        lcd.setCursor(9,1);
+        lcd.print(total);
+        last_total = total;
       }
     }
 
     for (int i=0; i<4; i++){raw_baht[i] = max(raw_baht[i], 0);} //limit raw baht data to minimum of 0
-    //Serial.print(raw_baht[1]);
-    //Serial.print(raw_baht[2]);
-    Serial.println(raw_baht[0]);
     vTaskDelay(10 / portTICK_PERIOD_MS); //delay for other tasks to continue
   }
 }
 
+void home_screen()
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("1:" + String(raw_baht[0]) + " ");
+  lcd.setCursor(5,0);
+  lcd.print("2:" + String(raw_baht[1]) + " ");
+  lcd.setCursor(11,0);
+  lcd.print("5:" + String(raw_baht[2]) + " ");
+  lcd.setCursor(0,1);
+  lcd.print("10:" + String(raw_baht[3]) + " ");
+  lcd.setCursor(7, 1);
+  lcd.print("t:" + String(total) + " ");
+}
+
 void task_setup()
 {
-  xTaskCreatePinnedToCore(button_handle,"button_handle", 1024, NULL, 2, NULL, 1);
-  xTaskCreatePinnedToCore(motor_handle,"motor_handle", 1024, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(button_handle,"button_handle", 4096, NULL, 2, NULL, 1);
+  xTaskCreatePinnedToCore(motor_handle,"motor_handle", 4096, NULL, 1, NULL, 1);
   xTaskCreatePinnedToCore(sensor_handle,"sensor_handle", 10000, NULL, 3, NULL, 1);
 }
 
@@ -221,4 +279,17 @@ void pin_setup()
   pinMode(motor_led, OUTPUT);
   pinMode(start_SW, INPUT_PULLUP);
   for (int i=0; i<4; i++){pinMode(sensor_pin[i], INPUT);}
+}
+
+void lcd_setup()
+{
+  lcd.init(I2C_SDA, I2C_SCL); // initialize the lcd to use user defined I2C pins
+	lcd.backlight();
+	lcd.setCursor(4,0);
+	lcd.print("Automatic");
+	lcd.setCursor(2,1);
+	lcd.print("Coin Counter");
+  delay(1000);
+  lcd.clear();
+  home_screen();
 }
--- src/pin_define.h
+++ src/pin_define.h
@@ -1,3 +1,5 @@
+
+//tmc2209 config
 #define stepper_current             700 //mA
 #define STALL_PIN                   26 // ESP diag pin is attached to
 #define SERIAL_PORT                 Serial2 // HardwareSerial port for ESP
@@ -11,17 +13,23 @@
 // Value of TCOOLTHRS must be greater than TSTEP & TPWMTHRS
 #define STALL_VALUE                 50// [0..255]
 
+//motor speed config
 #define forware_speed               500
 #define after_jamm_forward_speed    300
 #define reverse_speed               500
 #define jamm_dir_delay              500
 
-
+//buttons and led pins
 #define start_SW                    12
 #define motor_led                   19
 
+//coin sensors pins
 #define baht1_pin                   25
-#define baht2_pin                   32
-#define baht5_pin                   33
+#define baht2_pin                   33
+#define baht5_pin                   32
 #define baht10_pin                  35
+
+//lcd config
+#define I2C_SDA                     21
+#define I2C_SCL                     22
 
